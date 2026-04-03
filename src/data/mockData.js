4c27fc2aaedf755bb7986eb3554e5be79e6a8788
Mohit Yadav <yadavmohit85692@gmail.com>
2026-04-03 10:31:11 +0530

    Add Premium dashboard UI

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -1,4 +1,4 @@
-// src/data/mockData.js
+
 export const transactions = [
     {
         id: "1",
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,13 @@
-// src/pages/Dashboard.jsx
+
 export default function Dashboard() {
     return (
-        <div className="min-h-screen bg-gray-100 p-6">
-            <h1 className="text-2xl font-bold">Finance Dashboard</h1>
+        <div className="min-h-screen px-6 py-6">
+
+            <div className="flex justify-between items-center mb-6">
+                <h1 className="text-3xl font-bold gradient-text">
+                    Finance Dashboard
+                </h1>
+            </div>
         </div>
     );
 }
